8ec8a5010a00fdc894459a1b7e23b0fe3a94cfdd
Tim4ukys <tim4ukys.dev@yandex.ru>
2023-07-10 04:47:31 +0700

    add first snake

--- Snake/Game.cpp
+++ Snake/Game.cpp
@@ -4,18 +4,24 @@
 #include <thread>
 
 CGame::CGame(core::Graphics::GraphicsEngine& ge, core::Audio::AudioEngine* pAE)
-    : m_AudioMan(pAE)
+    : m_AudioMan(pAE), m_timerSnakeUpdate(250)
 {
     ge.onInitDevice.connect([&](ID3D11Device* pDevice, ID3D11DeviceContext* pDeviceContext) {
         m_pBox = std::make_unique<core::Graphics::Box>(&ge, 0.f, 0.f, 520.f, 520.f, 10.f, DirectX::Colors::Gray);
-
+        m_pSnake = std::make_unique<Snake>(ge);
         });
     ge.onRender.connect([&](ID3D11Device* pDevice, ID3D11DeviceContext* pDeviceContext) {
         m_AudioMan.proc();
         ge.setShaders();
 
-        if (m_gameState != GAME_STATE::FirstStart)
+        if (m_gameState != GAME_STATE::FirstStart) {
+            m_pSnake->draw();
             m_pBox->draw();
+            if (m_gameState == GAME_STATE::Play && bool(m_timerSnakeUpdate)) {
+                m_pSnake->next();
+                m_timerSnakeUpdate.reset();
+            }
+        }
 
         renderGUI();
         });
@@ -35,9 +41,11 @@ void CGame::renderGUI() {
         ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
         ImGui::Begin("main_menu", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoResize);
 
-        if (buttonDraw(m_gameState == GAME_STATE::FirstStart ? "start" : "continue", 1, 2))
+        if (buttonDraw(m_gameState == GAME_STATE::FirstStart ? "start" : "continue", 0, 2, m_bMainMenuHovOld)) {
             m_gameState = GAME_STATE::Play;
-        if (buttonDraw("exit", 2, 2))
+            m_timerSnakeUpdate.reset();
+        }
+        if (buttonDraw("exit", 1, 2, m_bMainMenuHovOld))
             PostQuitMessage(EXIT_SUCCESS);
 
         ImGui::End();
@@ -47,10 +55,10 @@ void CGame::renderGUI() {
     ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
 }
 
-bool CGame::buttonDraw(std::string_view text, size_t i, size_t count)
+bool CGame::buttonDraw(std::string_view text, size_t i, size_t count, bool* arrBtnOldHov)
 {
     auto size = ImGui::CalcTextSize(text.data());
-    auto pos = ImVec2(ImGui::GetIO().DisplaySize.x / 2 - size.x / 2, (ImGui::GetIO().DisplaySize.y / (count + 1)) * i - size.y / 2);
+    auto pos = ImVec2(ImGui::GetIO().DisplaySize.x / 2 - size.x / 2, (ImGui::GetIO().DisplaySize.y / (count + 1)) * (i + 1) - size.y / 2);
 
     ImGui::SetCursorPos(pos);
     bool res = ImGui::InvisibleButton(text.data(), size);
@@ -59,15 +67,15 @@ bool CGame::buttonDraw(std::string_view text, size_t i, size_t count)
     ImGui::SetCursorPos(pos);
     ImGui::TextColored(hov ? COLOR_MENU_HOV : COLOR_MENU_UNHOV, text.data());
 
-    if (hov && !m_btnOldHov[text]) {
-        m_btnOldHov[text] = true;
+    if (hov && !arrBtnOldHov[i]) {
+        arrBtnOldHov[i] = true;
         m_AudioMan.playFile("menu_select");
     }
     else if (res) {
         m_AudioMan.playFile("menu_direct");
     }
     else if (!hov) {
-        m_btnOldHov[text] = false;
+        arrBtnOldHov[i] = false;
     }
     return res;
 }
@@ -76,11 +84,57 @@ void CGame::wndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg) {
     case WM_KEYDOWN:
-        if (m_gameState == GAME_STATE::Play) m_gameState = GAME_STATE::Paused;
+        if (wParam == VK_ESCAPE && m_gameState == GAME_STATE::Play)
+            m_gameState = GAME_STATE::Paused;
+        else if (wParam == VK_UP) m_pSnake->setDirect<Snake::DIRECT::UP>();
+        else if (wParam == VK_DOWN) m_pSnake->setDirect<Snake::DIRECT::DOWN>();
+        else if (wParam == VK_LEFT) m_pSnake->setDirect<Snake::DIRECT::LEFT>();
+        else if (wParam == VK_RIGHT) m_pSnake->setDirect<Snake::DIRECT::RIGHT>();
         break;
     case WM_ACTIVATE:
         if (LOWORD(wParam) == WA_INACTIVE && m_gameState == GAME_STATE::Play)
             m_gameState = GAME_STATE::Paused;
         break;
+    }
+}
+
+void CGame::Snake::draw() {
+    stBox box = m_arrBoxes[0];
+    m_head.setPos(float(box.x) * SIZE_BOX + 10.f, float(box.y) * SIZE_BOX + 10.f);
+    m_head.draw();
+    for (size_t i{1}; i < m_nLen; ++i) {
+        box = m_arrBoxes[i];
+        m_body.setPos(float(box.x) * SIZE_BOX + 10.f, float(box.y) * SIZE_BOX + 10.f);
+        m_body.draw();
+    }
+}
+
+void CGame::Snake::next() {
+    if (m_bGrow) {
+        m_arrBoxes[m_nLen] = m_arrBoxes[m_nLen-1];
+    }
+    for (size_t i{m_nLen-1}; i > 0; --i) {
+        m_arrBoxes[i] = m_arrBoxes[i-1];
+    }
+    if (m_bGrow) {
+        m_nLen++;
+        m_bGrow = false;
+    }
+    if (m_nSizeStackDirects) {
+        m_direct = m_stackDirects[0];
+        for (size_t i{m_nSizeStackDirects-1}; i > 0; --i) {
+            m_stackDirects[i-1] = m_stackDirects[i];
+        }
+        m_nSizeStackDirects--;
+    }
+
+    if (m_direct == DIRECT::RIGHT) {
+        if (++m_arrBoxes[0].x == COUNT_BOX) m_arrBoxes[0].x = 0;
+    } else if (m_direct == DIRECT::LEFT) {
+        if (--m_arrBoxes[0].x < 0) m_arrBoxes[0].x = COUNT_BOX-1;
+    } else if (m_direct == DIRECT::UP) {
+        if (++m_arrBoxes[0].y == COUNT_BOX) m_arrBoxes[0].y = 0;
+    } else {
+        if (--m_arrBoxes[0].y < 0) m_arrBoxes[0].y = COUNT_BOX-1;
     }
 }
--- Snake/Game.hpp
+++ Snake/Game.hpp
@@ -7,22 +7,70 @@
 #include <functional>
 
 #include "AudioMan.hpp"
+#include "Timer.hpp"
 
 class CGame {
     std::unique_ptr<core::Graphics::Box> m_pBox{};
 
-    static constexpr int COUNT_BOX = 20;
+    static constexpr int COUNT_BOX = 34;
     static constexpr float SIZE_BOX = (520.f - 20.f) / float(COUNT_BOX);
-    /*class button {
-        bool m_bOldHov{};
+
+    struct stBox {
+        int x{}, y{};
+    };
+
+    class Snake {
+        core::Graphics::SolidRect m_head, m_body;
+        size_t m_nLen{};
+        stBox m_arrBoxes[COUNT_BOX*COUNT_BOX];
+        bool m_bGrow{};
+
     public:
+        enum class DIRECT {
+            UP, DOWN, LEFT, RIGHT
+        };
+        template<DIRECT direct>
+        void setDirect() {
+            if (m_nSizeStackDirects >= ARRAYSIZE(m_stackDirects)) return;
+            if constexpr (direct == DIRECT::UP) {
+                if (m_stackDirects[m_nSizeStackDirects != 0 ? m_nSizeStackDirects-1 : 0] != DIRECT::DOWN)
+                    m_stackDirects[m_nSizeStackDirects++] = direct;
+            } else if constexpr (direct == DIRECT::DOWN) {
+                if (m_stackDirects[m_nSizeStackDirects != 0 ? m_nSizeStackDirects-1 : 0] != DIRECT::UP)
+                    m_stackDirects[m_nSizeStackDirects++] = direct;
+            } else if constexpr (direct == DIRECT::LEFT) {
+                if (m_stackDirects[m_nSizeStackDirects != 0 ? m_nSizeStackDirects-1 : 0] != DIRECT::RIGHT)
+                    m_stackDirects[m_nSizeStackDirects++] = direct;
+            } else {
+                if (m_stackDirects[m_nSizeStackDirects != 0 ? m_nSizeStackDirects-1 : 0] != DIRECT::LEFT)
+                    m_stackDirects[m_nSizeStackDirects++] = direct;
+            }
+        }
 
-        bool draw(std::string_view text, size_t i, size_t count, AudioMan& am);
-    };*/
-    //bool m_bBtnOldHov{};
-    std::map<std::string_view, bool> m_btnOldHov{};
-    bool buttonDraw(std::string_view text, size_t i, size_t count);
+        explicit Snake(core::Graphics::GraphicsEngine& ge)
+            : m_head(&ge, 0, 0, SIZE_BOX, SIZE_BOX, DirectX::Colors::DarkOliveGreen),
+              m_body(&ge, 0, 0, SIZE_BOX, SIZE_BOX, DirectX::Colors::DarkSeaGreen) {
+            m_nLen = 3;
+            m_arrBoxes[0] = { 1, 3 };
+            m_arrBoxes[1] = { 1, 2 };
+            m_arrBoxes[2] = { 1, 1 };
+        }
 
+        void next();
+        inline void grow() noexcept { m_bGrow = true; }
+
+        void draw();
+
+    private:
+        DIRECT m_direct = DIRECT::LEFT;
+        DIRECT m_stackDirects[2]{DIRECT::LEFT};
+        size_t m_nSizeStackDirects = 0;
+    };
+    std::unique_ptr<Snake> m_pSnake{};
+    Timer m_timerSnakeUpdate;
+
+    bool m_bMainMenuHovOld[2];
+    bool buttonDraw(std::string_view text, size_t i, size_t count, bool* arrBtnOldHov);
 
     enum class GAME_STATE {
         FirstStart,
